a6462a7104baec12c7b185e86ab314e3c36bfa23
am121-dwc <aoimorisugi121@gmail.com>
2024-03-09 10:22:41 +0000

    faker and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  # ユーザー用
+  # URL /user/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
