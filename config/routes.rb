8df1caa518cf33efadd0643915b96e7a4317ba35
am121-dwc <aoimorisugi121@gmail.com>
2024-03-10 09:38:51 +0000

    admin or user

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/show'
   resources :books, only: [:index, :new, :edit, :update, :create]
-  root to: 'homes#top'
+  root to: 'homes#about'
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
