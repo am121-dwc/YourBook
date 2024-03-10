40e189a1e097553b6867a8749dacddabff0259e3
am121-dwc <aoimorisugi121@gmail.com>
2024-03-10 03:07:12 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  def update
+  end
+
+  def create
+  end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,16 @@
 class FavoritesController < ApplicationController
+  def create
+
+  end
+
+  def destroy
+
+  end
+  def create_book_comment
+    
+  end
+  
+  def destoy_book_comment
+    
+  end
 end
