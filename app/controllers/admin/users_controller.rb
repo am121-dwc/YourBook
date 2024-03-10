100eef34de10c0a1e86ad57faff44608fcaef479
am121-dwc <aoimorisugi121@gmail.com>
2024-03-10 08:42:35 +0000

    add

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,9 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
   def destroy
-
+    user = user.find(params[:id])
+    comment.destroy
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,40 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
-  end
-  def update
+    @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was updated."
+    else
+      flash.now[:notice] = "Book was not updated."
+      reuest.referer
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "Book was created."
+    else
+      flash.now[:notice] = "Book was not created."
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :introduction)
+  end
 end
