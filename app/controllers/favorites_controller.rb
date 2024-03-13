82f21bf0b9a3fb7376c102c272053af6f53f8926
am121-dwc <aoimorisugi121@gmail.com>
2024-03-13 07:45:05 +0000

    add

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -17,8 +17,6 @@ class FavoritesController < ApplicationController
     @book = @comment.book
     favorite = current_user.favorites.new(book_id: @book.id, book_comment_id: @comment.id)
     favorite.save
-    # 非同期化ができていないためhtmlでの処理です。
-    redirect_to request.referer
   end
 
   def destroy_book_comment
@@ -26,7 +24,5 @@ class FavoritesController < ApplicationController
     @book = @comment.book
     favorite = current_user.favorites.find_by(book_id: @book.id, book_comment_id: @comment.id)
     favorite.destroy
-    # 非同期化ができていないためhtmlでの処理です。
-    redirect_to request.referer
   end
 end
