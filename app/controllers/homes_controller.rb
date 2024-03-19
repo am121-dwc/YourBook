e324a28663f2f9039a3a5821f5422f54e4596cbe
am121-dwc <aoimorisugi121@gmail.com>
2024-03-19 15:53:30 +0000

    bug fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,5 @@
 class HomesController < ApplicationController
   def top
-    # いいね数による並び替え
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorites).sort_by { |book| -book.favorites.where(created_at: from...to).count }
-    @book = Book.new
   end
   def about
   end
