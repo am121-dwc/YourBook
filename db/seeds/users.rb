2478484ada1d631e907b4fdcedafd05a087d4d06
am121-dwc <aoimorisugi121@gmail.com>
2024-03-24 07:16:58 +0000

    layouts

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,6 +1,6 @@
 puts 'users ...'
 20.times do
-  user = User.create!(
+    user = User.create!(
     name: Faker::Name.unique.name,
     email: Faker::Internet.unique.email,
     introduction: Faker::Lorem.paragraph(sentence_count: 2),
